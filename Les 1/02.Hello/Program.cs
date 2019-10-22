e7fee7b470123e0ccaf8efaa9bdfa2f36b3a4cf9
Rjibaraoko <junior4553@hotmail.fr>
2019-10-22 15:30:05 +0200

    oefening 1 Hello soluction

--- Les 1/02.Hello/Program.cs
+++ Les 1/02.Hello/Program.cs
@@ -10,6 +10,16 @@ namespace _02.Hello
     {
         static void Main(string[] args)
         {
+            string userInput;
+            Console.WriteLine("Hello Wereld! Wat is jouw naam?");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine("Aangename kennismaking'{0}'", userInput);
+            Console.WriteLine("Druk op een knop om verder te gaan....");
+
+
+            Console.ReadKey();
+
         }
     }
 }
