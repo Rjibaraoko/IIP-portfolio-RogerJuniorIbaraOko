63e398795dc3870c11e55601d8bbccf09188f3e7
Rjibaraoko <junior4553@hotmail.fr>
2019-10-25 17:26:44 +0200

    les 2 oefening 2 begin

---
+++ Les 2/04.console.Raden/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.console.Raden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Raad een getal tussen 1 en 10");
+
+            int getal = 8;
+            int input ;
+
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (getal > input)
+            {
+                Console.WriteLine("Je raad was te hoog!");
+
+            }
+
+            else if (getal < input) {
+
+                Console.WriteLine("Je raad was te laag!");
+            };
+
+            Console.ReadLine();
+
+        }
+    }
+}
